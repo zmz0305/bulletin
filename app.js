6cb724ba3cd32d6979f8938d23213b813f827518
zmz0305 <zmz305@gmail.com>
2015-01-08 17:05:40 -0800

    complete

--- app.js
+++ app.js
@@ -4,15 +4,32 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
+var http = require("http").createServer(app)
+  , io = require("socket.io").listen(http)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+/* 
+  The list of participants in our chatroom.
+  The format of each participant will be:
+  {
+    id: "sessionId",
+    name: "participantName"
+  }
+*/
+app.set('io', io);
+var participants = [];
+//set server ip address
+app.set('ipaddr', '127.0.0.1');
+app.set('port', 8080);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -51,10 +68,79 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
+        message: "err.message",
         error: {}
     });
 });
 
+
+//Handle route "GET /", as in "http://localhost:8080/"
+app.get("/", function(request, response) {
+
+  //Return what's on your mind
+  response.json(200, {message: "express is cool"});
+
+});
+
+
+
+/* Socket.IO events */
+app.get('io').on("connection", function(socket){
+
+  /*
+    When a new user connects to our server, we expect an event called "newUser"
+    and then we'll emit an event called "newConnection" with a list of all 
+    participants to all connected clients
+  */
+  socket.on("newUser", function(data) {
+    participants.push({id: data.id, name: data.name});
+    io.sockets.emit("newConnection", {participants: participants});
+  });
+
+  /*
+    When a user changes his name, we are expecting an event called "nameChange" 
+    and then we'll emit an event called "nameChanged" to all participants with
+    the id and new name of the user who emitted the original message
+  */
+  socket.on("nameChange", function(data) {
+    _.findWhere(participants, {id: socket.id}).name = data.name;
+    io.sockets.emit("nameChanged", {id: data.id, name: data.name});
+  });
+
+  /* 
+    When a client disconnects from the server, the event "disconnect" is automatically 
+    captured by the server. It will then emit an event called "userDisconnected" to 
+    all participants with the id of the client that disconnected
+  */
+  socket.on("disconnect", function() {
+    participants = _.without(participants,_.findWhere(participants, {id: socket.id}));
+    io.sockets.emit("userDisconnected", {id: socket.id, sender:"system"});
+  });
+
+});
+
+routes.post("/message", function(request, response) {
+
+  console.log(typeof(app.get('io').sockets));
+  //The request body expects a param named "message"
+  var message = request.body.message;
+
+  //If the message is empty or wasn't sent it's a bad request
+  if(_.isUndefined(message) || _.isEmpty(message.trim())) {
+    return response.json(400, {error: "Message is invalid"});
+  }
+    //We also expect the sender's name with the message
+  var name = request.body.name;
+
+  //Let our chatroom know there was a new message
+  app.get('io').sockets.emit("incomingMessage", {message: message, name: name});
+
+  //Looks good, let the client know
+  response.json(200, {message: "Message received"});
+});
+
+http.listen(8080, app.get("ipaddr"), function() {
+  console.log("Server up and running. Go to http://" + app.get("ipaddr") + ":" + app.get("port"));
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var app = require('../app');
 var router = express.Router();
+var _ = require('underscore');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+//POST method to create a chat message
+
+
+router.post("/test", function(req, res){
+	res.send("aaaa");
+});
+
 
 module.exports = router;
